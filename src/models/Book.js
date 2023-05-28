06959c7e01f7ecf2f23df5fd3966cbfb3cfa7167
AntoineLRK <admin@antoinelrk.com>
2023-05-28 08:04:09 +0200

    Prepare Book model

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,9 +1,43 @@
+import { Schema } from "mongoose"
+
 let Model
 
 const _init = (db) => {
-    Model = db.model(`${import.meta.url.split('/').pop().split('.').shift()}`, {
-        title: String
+    const schema = new Schema({
+        userId: {
+            type: String
+        },
+        title: {
+            type: String
+        },
+        author: {
+            type: String
+        },
+        imageUrl: {
+            type: String
+        },
+        year: {
+            type: Number
+        },
+        genre: {
+            type: String
+        },
+        ratings: [
+            {
+                userId: {
+                    type: String
+                },
+                grade: {
+                    type: Number
+                }
+            }
+        ],
+        averageRating: {
+            type: Number
+        }
     })
+    Model = db.model(`${import.meta.url.split('/').pop().split('.').shift()}`, schema)
+    Model.createCollection()
 }
 
 const get = () => Model
